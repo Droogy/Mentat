ba7d687cf8c2d816225c7ed069b5b4e91c50e9a6
Droogy <drooogy@pm.me>
2021-03-21 21:29:37 -0400

    Update mentat.sh
    added outbound connection module, added loudLogin module and re-designed file hasher

--- mentat.sh
+++ mentat.sh
@@ -9,16 +9,29 @@
 # check login times and users with "check users"
 # "honey port" sets up a bash honeypot with a fake banner we can customize
 
-# version 1.0
+# version 1.1
+
+#!/bin/bash
 
 # COLORS
+
+cyan="\033[0;36m"
 green="\033[0;32m"
 red="\033[0;31m"
 nocolor="\033[0;0m"
 
+ColorCyan(){
+	echo -ne $cyan$1$nocolor
+}
+ColorGreen(){
+	echo -ne $green$1$nocolor
+}
+ColorRed(){
+	echo -ne $red$1$nocolor
+}
 # lets check if we have certain folders and then create them
-# check if hashes folder does NOT exist
 function infra {
+	# check if hashes folder does NOT exist
 	if [ ! -d "baseline/" ]
 	then
 		mkdir baseline
@@ -44,7 +57,9 @@ function baseline_files {
 }
 
 function new_files {
+	clear
 	printf "${green}[*] Re-hashing and checking baseline for: $1\n\r${nocolor}"
+	sleep .5
 	find "/$1" -type f -exec md5sum "{}" + > hashes/"$1".txt
 	diff baseline/"$1".txt hashes/"$1".txt 1>/dev/null; ec=$?
 
@@ -53,40 +68,7 @@ function new_files {
 	else
 		diff baseline/"$1".txt hashes/"$1".txt
 	fi
-}
-
-function checker {
-	printf "Which directory to check for mischief? \n\r"
-	options=("/tmp" "/var" "/etc" "/bin" "/home" "/dev" "Quit")
-	select opt in "${options[@]}"
-	do
-		case $opt in
-			"/tmp")
-				new_files tmp
-				;;
-			"/var")
-				new_files var
-				;;
-			"/etc")
-				new_files etc
-				;;
-			"/bin")
-				new_files bin
-				;;
-			"/home")
-				new_files home
-				;;
-			"/dev")
-				new_files dev
-				;;
-			"Quit")
-				break
-				;;
-			*)
-				echo "Invalid Answer"
-				;;
-		esac
-	done
+	sleep 3
 }
 
 # check who's logged in and return when they logged in
@@ -101,7 +83,7 @@ function nullPit {
 	read -p "Which port do you wanna make the pit run on? [1000-65535]" port
 	bdir="./.nullpit"
 	banner="You found my backdoor \n\rtty1\r\n\rbackdoor login:"
-	if [ ! -d "/root/.nullpit" ]; then
+	if [ ! -d "./.nullpit" ]; then
 		mkdir ./.nullpit
 		touch ./.nullpit/$port.txt
 	fi
@@ -125,36 +107,51 @@ function cleanup {
 	rm -rf .nullpit/
 }
 
-function main_app {
-	PS3="Please select your choice (or press enter): "
-	options=("baseline" "check users" "check files" "cleanup" "honey port" "Quit")
-	select opt in "${options[@]}"
-	do
-		case $opt in
-			"baseline")
-				infra;
-				baseline_files;
-				;;
-			"check users")
-				snoopy;
-				;;
-			"check files")
-				checker;
-				;;
-			"cleanup")
-				cleanup;
-				;;
-			"honey port")
-				nullPit;
-				;;
-			"Quit")
-				break;
-				;;
-			*)
-				echo "Invalid Answer"
-				;;
-		esac
-	done
+# watch for all outgoing connections
+# by monitoring the SYN bit set which is tcp[13] == 2
+# we can make the tcpdump buffered with -l to grep stdout as well
+function outWatch {
+	ip=`ifconfig | grep broadcast | tr -s ' ' | cut -d ' ' -f 3`
+	tcpdump -l -i any src host $ip and tcp[13]==2 | \
+		awk '{print "A user tried connecting to: "$5}'
 }
 
-main_app
+# let's make it loud and annoying to login
+# we'll just echo some stuff into users' .profile files
+function loudLogin {
+	find /home -type f -name ".profile" -exec \
+		echo -ne "wall 'hey everyone $USER just logged in!' 
+		while [1]; do
+			sleep 10
+			Connection lost...attempting to reconnect
+		done &" >> {} \;
+}	
+		
+function main_app_v2 {
+echo -ne "
+$(ColorCyan 'Mentat:')
+$(ColorGreen '1)') Take baseline *Do this first*
+$(ColorGreen '2)') Check for user logins
+$(ColorGreen '3)') Check files for change
+$(ColorGreen '4)') Cleanup Files
+$(ColorGreen '5)') Make logins loud for users
+$(ColorGreen '6)') Create a honeypot
+$(ColorGreen '7)') Watch for outgoing connections
+$(ColorGreen '8)') $(ColorRed 'Quit')
+$(ColorGreen 'Choose:')"
+		read a
+		case $a in
+			1) infra && baseline_files ; main_app_v2 ;;
+			2) snoopy; main_app_v2 ;;
+			3) new_files tmp; new_files var; new_files home; new_files root; \
+				new_files bin; main_app_v2 ;;
+			4) cleanup; main_app_v2 ;;
+			5) loudLogin; main_app_v2 ;;
+			6) nullPit; main_app_v2 ;;
+			7) outWatch; main_app_v2 ;;
+			8) exit 0;;
+			*) echo "Invalid Answer" ; main_app_v2 ;;
+		esac
+}
+
+main_app_v2
